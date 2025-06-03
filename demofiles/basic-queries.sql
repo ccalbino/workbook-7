a01842a7e440b9992dc54132e94ef7a035ef633e
ccalbino <christian.albino89@gmail.com>
2025-06-03 10:31:51 -0400

    test 2

---
+++ demofiles/basic-queries.sql
@@ -0,0 +1,21 @@
+-- 1
+select 
+	ProductName
+from
+	products;
+    
+-- 2 
+select
+	ProductID, ProductName, UnitPrice
+from
+	products;
+-- 3
+select
+	ProductID, ProductName, UnitPrice
+from 
+	products
+Order by
+	UnitPrice;
+
+	
+	
